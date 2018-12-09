30a8485dc0ec1b2e17833f6d485158d8e5ebd0f9
xgrafix <xgrafix@gmail.com>
2018-12-10 00:19:05 +0300

    New commit with commands

--- Code/main.py
+++ Code/main.py
@@ -1,11 +1,12 @@
 # This is my first python on github.
-#
+# Mastering PEP8 styling.
 #
 
 a = []
 
 def main():
     print('Hello World, Github!')
+
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python_Lists
 Training to Python Lists.
+
+# Trying to master Lists and PEP 8 at the same time.
