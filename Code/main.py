0eabd3197270c8484c20805dd95f5ed871d82d0e
xgrafix <xgrafix@gmail.com>
2018-12-10 14:14:41 +0300

    New changes

--- Code/main.py
+++ Code/main.py
@@ -1,11 +1,27 @@
 # This is my first python on github.
 # Mastering PEP8 styling.
-#
+# Using 78 characters for code and 72 characters for comments.
 
-a = []
+
+# Initialization
+var_one = 0
+var_two = 0
+var_three = 0
+var_four = 0
+
+
+def long_function_name(a, b, c, d):
+    pass
+
+
+# Aligned with opening delimiter.
+foo = long_function_name(var_one, var_two,
+                         var_three, var_four)
+
 
 def main():
     print('Hello World, Github!')
+    print(foo)
 
 
 if __name__ == '__main__':
